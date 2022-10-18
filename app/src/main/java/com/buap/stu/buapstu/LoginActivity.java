39a8bce8616a437d8f9c5109d0694748284062fd
SalvadorMendoza96 <ceinnova12112@innovaccion.mx>
2022-10-17 21:45:55 -0500

    Inicio de sesion y registro

--- app/src/main/java/com/buap/stu/buapstu/LoginActivity.java
+++ app/src/main/java/com/buap/stu/buapstu/LoginActivity.java
@@ -1,31 +1,74 @@
 package com.buap.stu.buapstu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     Button ini_sesion;
+    private EditText correo2;
+    private EditText contrasena;
+    private FirebaseAuth mAuth;
 
-    private void goToPrincipal() {
+    String mail = "";
+    String password = "";
+
+    /*private void goToPrincipal() {
         Intent intent = new Intent(LoginActivity.this,PrincipalActivity.class);
         startActivity(intent);
-    }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ini_sesion = (Button) findViewById(R.id.ini_sesion);
+        mAuth = FirebaseAuth.getInstance();
+        correo2 = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.Password);
 
-        ini_sesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToPrincipal();
-            }
-        });
     }
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    //Con este metodo podremos hacer la conexion entre el firebase y la aplicacion
+    public void iniciarSesion(View view){
+
+        mAuth.signInWithEmailAndPassword(correo2.getText().toString(), contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            Intent intent = new Intent(LoginActivity.this,PrincipalActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Usuario o contraseña no valido",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/buap/stu/buapstu/RegistroActivity.java
+++ app/src/main/java/com/buap/stu/buapstu/RegistroActivity.java
@@ -1,13 +1,31 @@
 package com.buap.stu.buapstu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -15,10 +33,31 @@ public class RegistroActivity extends AppCompatActivity {
     LinearLayout formContainerDriver,LLImaButtons,DriverContainer,StudentContainer,formContainerStuden,LLContBTNResgitro;
     TextView mensajeRegistro;
 
+    private EditText correo, contrasena, nombre, matricula;
+    private Button btnRegistrarse;
+
+    private String name= "";
+    private String mail= "";
+    private String mat= "";
+    private String password= "";
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        correo = findViewById(R.id.AEmail);
+        contrasena =findViewById(R.id.APassword);
+        nombre = findViewById(R.id.ANombreCompleto);
+        matricula = findViewById(R.id.AMAtricula);
+        btnRegistrarse =  findViewById(R.id.BTNRegistro);
+
 
         conductor=(ImageButton) findViewById(R.id.imageButtonDriver);
         students=(ImageButton) findViewById(R.id.imageButtonStudent);
@@ -54,5 +93,62 @@ public class RegistroActivity extends AppCompatActivity {
                 LLContBTNResgitro.setVisibility(View.VISIBLE);
             }
         });
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = nombre.getText().toString();
+                mat = matricula.getText().toString();
+                mail = correo.getText().toString();
+                password = contrasena.getText().toString();
+
+                if(!name.isEmpty() && !mail.isEmpty() && !password.isEmpty() && !mat.isEmpty()){
+                    if(password.length()>=6){
+                        registrarUsuario();
+                    }
+                    else{
+                        Toast.makeText(RegistroActivity.this, "El password debe tener almenos 6 caracteres", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+    }
+
+    //Metodo que nos hara registrarnos en la firebase
+    private void registrarUsuario(){
+
+        mAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Map<String, Object> datosUsuario = new HashMap<>();
+                    datosUsuario.put("Nombre", name);
+                    datosUsuario.put("Email", mail);
+                    datosUsuario.put("Matricula", mat);
+                    datosUsuario.put("Contraseña", password);
+
+                    String id = mAuth.getCurrentUser().getUid();
+                    mDatabase.child("Usuarios").child(id).setValue(datosUsuario).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if(task2.isSuccessful()){
+                                startActivity(new Intent(RegistroActivity.this, LoginActivity.class));
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(RegistroActivity.this, "No se pudieron crear los datos correctamente", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error al crear usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
